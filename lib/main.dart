ec44940b8e7d90ccd5510126e9532a92609ed597
varadmondkar <varadmondkar@gmail.com>
2019-03-31 00:42:00 +0530

    Timer study using aviacommerce's flutter tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,123 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "StatefulWidget Example", home: MyStatefulWidget());
+  State<StatefulWidget> createState() => TimerState();
+}
+
+class TimerState extends State<MyApp> {
+  static const duration = const Duration(seconds: 1);
+
+  int secondsPassed = 0;
+  bool isActive = false;
+
+  Timer timer;
+
+  void handleTick() {
+    if (isActive) {
+      setState(() {
+        secondsPassed += 1;
+      });
+    }
   }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
 
   @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<StatefulWidget> {
-  String displayText = "Welcome to Flutter Application";
-  int _counter = 0;
-
-  void updateText() {
-    _counter++;
-    setState(() {
-      displayText = "Thanks for your click $_counter.";
+  void initState() {
+    timer = Timer.periodic(duration, (Timer t) {
+      handleTick();
     });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("StatefulWidget Example"),
-      ),
-      body: Center(
-        child: Text(displayText),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        tooltip: "Click to update the text",
-        child: Icon(Icons.adb),
-        onPressed: updateText,
-        backgroundColor: Colors.teal,
+    int seconds = secondsPassed % 60;
+    int minutes = secondsPassed ~/ 60;
+    int hours = secondsPassed ~/ (60 * 60);
+
+    return MaterialApp(
+        title: "Timer App Example",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Timer Example"),
+            ),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextContainer(
+                      label: 'HRS',
+                      value: hours.toString().padLeft(2, '0'),
+                    ),
+                    TextContainer(
+                      label: 'MINS',
+                      value: minutes.toString().padLeft(2, '0'),
+                    ),
+                    TextContainer(
+                      label: 'SECS',
+                      value: seconds.toString().padLeft(2, '0'),
+                    )
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.all(20.0),
+                  child: RaisedButton(
+                    padding: EdgeInsets.all(10.0),
+                    onPressed: () {
+                      setState(() {
+                        isActive = !isActive;
+                      });
+                    },
+                    child: Text(
+                      isActive ? 'STOP' : 'START',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                )
+              ],
+            ))));
+  }
+}
+
+class TextContainer extends StatelessWidget {
+  TextContainer({this.label, this.value});
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+      padding: EdgeInsets.all(25.0),
+      decoration: BoxDecoration(
+          color: Colors.teal, borderRadius: BorderRadius.circular(5.0)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            '$value',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '$label',
+            style: TextStyle(
+                color: Colors.white70,
+                fontSize: 15.0,
+                fontWeight: FontWeight.normal),
+          )
+        ],
       ),
     );
   }
